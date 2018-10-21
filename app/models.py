36262b5431545713fd0655b70a52a042daebe614
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-10-22 00:28:11 +0300

    add file email.py

---
+++ app/models.py
@@ -0,0 +1,58 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__= 'users'
+    id = db.Column(db.Integer,  primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True, index =True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    post = db.relationship('Blogpost',backref='user',lazy='dynamic')
+    comments = db.relationship('Comment', backref = 'comment', lazy= "dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'{self.username}'
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer,primary_key=True)
+    comment_content = db.Column(db.String())
+    post_id = db.Column(db.Integer,db.ForeignKey('post.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+
+    def __repr__(self):
+        return f'{self.comment}'
+
+class Blogpost(db.Model):
+    __tablename__='post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+    post_id = db.relationship('Comment', backref = 'comments', lazy= "dynamic")
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
